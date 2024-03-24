f5575590aa0919654504a9ca09dba3dcfd40521b
Maxim E <maermolenko@mail.ru>
2024-03-24 20:23:18 +0300

    Game Over logic added

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -87,6 +87,13 @@ export default class GameController {
     this.initiateCharacters();
   }
 
+  gameOver() {
+    this.newGame();
+    this.gamePlay.modal.classList.add('modal_visibility');
+    let text = this.gamePlay.modal.querySelector('h1');
+    text.innerHTML = 'Game Over';
+  }
+
   modalResponse(event) {
     if (event.target.id == 'new-game') {
       this.gamePlay.modal.classList.remove('modal_visibility');
@@ -125,54 +132,6 @@ export default class GameController {
     }});
     return isSelected;
   }
-
-  /*getActionsRange(flag, char = this.focusedChar) {
-    if (this.focusedChar) {
-      let index = char.position;
-      let range = 0;
-      let board = this.gamePlay.boardSize
-      if (flag == 'move'){
-        range = char.character.moveRange;
-      }
-      else if (flag == 'attack') {
-        range = char.character.attackRange;
-      }
-      else {
-        throw new SyntaxError('Choose proper action flag (move or attack)!')
-      }
-      let cellsToAct = [];
-      for (let i = 0; i < board; i += 1){
-        let currentRow = [];
-        let row = i*8
-        for (let cell = 0 + row; cell < 8 + row; cell += 1) {
-          currentRow.push(cell);
-        }
-        cellsToAct.push(currentRow);
-      }
-      let charRowIndex = cellsToAct.findIndex(row => row.includes(index));
-      let charInRowIndex = (cellsToAct.filter(row => row.includes(index)).flat()).indexOf(index);
-      let startRow = charRowIndex < range ? 0 : charRowIndex - range;
-      if (board - charRowIndex - 1 > range){
-        let endRow = startRow == 0 ? charRowIndex + range + 1 : 2*range + 1;
-        cellsToAct = cellsToAct.splice(startRow, endRow)
-      }
-      else {
-        cellsToAct = cellsToAct.splice(startRow)
-      }
-      let startCell = charInRowIndex - range > 0 ? charInRowIndex - range : 0;
-      if (board - charInRowIndex - 1 > range){
-        let endCell = startCell == 0 ? charInRowIndex + range + 1 : 2*range + 1;
-        cellsToAct.forEach((r, i) => {cellsToAct[i] = r.splice(startCell, endCell)});
-      }
-      else {
-        cellsToAct.forEach((r, i) => {cellsToAct[i] = r.splice(startCell)});
-      }
-      return cellsToAct.flat()
-    }
-    else {
-      return []
-    }
-  }*/
 
   getPositionedVillians() {
     let positionedVillians = [];
@@ -278,6 +237,9 @@ export default class GameController {
       this.charsArray = this.charsArray.filter(char => char.position != attackedHero.position);
       this.gamePlay.redrawPositions(this.charsArray)
       }
+    if ((this.getPositionedHeroes()).length == 0) {
+      this.gameOver();
+    }
          
   }
 
